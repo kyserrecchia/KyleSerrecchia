2ad333734abc85ab56094a1aef6f6d6cb564e910
Kyle Serrecchia <kyserrecchia@gmail.com>
2018-04-08 16:23:29 -0700

    added about section

--- public/index.js
+++ public/index.js
@@ -1,14 +1,24 @@
 
 //NAVIGATION//////////////////////////////////////////////////////////////////////////////////////
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+const sideNav = document.getElementById('mySidenav');
+offNavListener();
+function offNavListener(){
+	sideNav.addEventListener('mouseleave', function(){
+		setTimeout(function(){closeNav()}, 1100);
+	});
 }
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+
+function openNav(){
+    sideNav.style.width = "250px";
 }
+function closeNav(){
+	sideNav.style.width = 0;
+}
+
 const scrollFunc = {
 	home: function(){document.querySelector('.home').scrollIntoView({behavior: 'smooth'})},
-	about: function(){document.querySelector('.about').scrollIntoView({behavior: 'smooth'})},
+	about: function(){document.querySelector('.about').scrollIntoView({behavior: 'smooth'});
+					  document.getElementById('downV').style.animationIterationCount = 0},
 	skills: function(){document.querySelector('.skills').scrollIntoView({behavior: 'smooth'})},
 	projects: function(){document.querySelector('.projects').scrollIntoView({behavior: 'smooth'})},
 	contact: function(){document.querySelector('.contact').scrollIntoView({behavior: 'smooth'})}
